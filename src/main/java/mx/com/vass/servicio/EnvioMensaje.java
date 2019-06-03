4776622825fb991ef54d6e375b0a3f0df12b8296
ingalopez27 <michel.lopez@vass.com.mx>
2019-06-03 16:10:28 -0500

    Se agrega el plugin de java, en la dependencia java

--- src/main/java/mx/com/vass/servicio/EnvioMensaje.java
+++ src/main/java/mx/com/vass/servicio/EnvioMensaje.java
@@ -6,32 +6,34 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EnvioMensaje {
+	private static final Logger logger = LoggerFactory.getLogger(EnvioMensaje.class);
+	private static final String QUEUE_NAME = "Envio mensaje a RabbitMQ";
 
-    private static final String QUEUE_NAME = "Envio mensaje a RabbitMQ";
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		logger.info("Log4j : Comienza envio de mensaje !!");
+		Connection conexion = factory.newConnection();
+		Channel canal = conexion.createChannel();
 
-    public static void main(String[] argv) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+		try {
+			canal.queueDeclare(QUEUE_NAME, false, false, false, null);
 
-        Connection conexion = factory.newConnection();
-        Channel canal = conexion.createChannel();
+			for (int i = 0; i < 15; ++i) {
+				String message = String.format("Hola, enviando mensaje RabbitMQ",
+						LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+				canal.basicPublish("", QUEUE_NAME, null, message.getBytes());
+				System.out.println(String.format("Envio de mensaje RabbitMQ", message));
 
-        try {
-            canal.queueDeclare(QUEUE_NAME, false, false, false, null);
-
-            for (int i = 0; i < 10; ++i) {
-                String message = String.format("Hola, enviando mensaje RabbitMQ", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
-                canal.basicPublish("", QUEUE_NAME, null, message.getBytes());
-                System.out.println(String.format("Envio de mensaje RabbitMQ", message));
-
-                Thread.sleep(1500);
-            }
-        } finally {
-            canal.close();
-            conexion.close();
-        }
-    }
+				Thread.sleep(1500);
+			}
+		} finally {
+			canal.close();
+			conexion.close();
+		}
+	}
 }
-
--- src/main/java/mx/com/vass/servicio/RecepcionMensaje.java
+++ src/main/java/mx/com/vass/servicio/RecepcionMensaje.java
@@ -1,37 +1,42 @@
 package mx.com.vass.servicio;
 
 import com.rabbitmq.client.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class RecepcionMensaje {
+	private static final Logger logger = LoggerFactory.getLogger(RecepcionMensaje.class);
+	private final static String QUEUE_NAME = "Mensaje de llegada a RabbitMQ";
 
-    private final static String QUEUE_NAME = "Mensaje de llegada a RabbitMQ";
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		logger.info("Log4j : Comienza envio de mensaje !!");
+		
+		Connection cone = factory.newConnection();
+		Channel can = cone.createChannel();
 
-    public static void main(String[] argv) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection cone = factory.newConnection();
-        Channel can = cone.createChannel();
+		try {
+			can.queueDeclare(QUEUE_NAME, false, false, false, null);
+			System.out.println(" [*] Esperando los mensajes. La salida es CTRL+C");
 
-        try {
-            can.queueDeclare(QUEUE_NAME, false, false, false, null);
-            System.out.println(" [*] Esperando los mensajes. La salida es CTRL+C");
+			Consumer consumer = new DefaultConsumer(can) {
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+						byte[] body) throws IOException {
+					String message = new String(body, "UTF-8");
+					System.out.println(String.format("Recepcion de mensaje", message));
+				}
+			};
 
-            Consumer consumer = new DefaultConsumer(can) {
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                    String message = new String(body, "UTF-8");
-                    System.out.println(String.format("Recepcion de mensaje", message));
-                }
-            };
+			can.basicConsume(QUEUE_NAME, true, consumer);
 
-            can.basicConsume(QUEUE_NAME, true, consumer);
-
-            Thread.sleep(20000);
-        } finally {
-            can.close();
-            cone.close();
-        }
-    }
+			Thread.sleep(20000);
+		} finally {
+			can.close();
+			cone.close();
+		}
+	}
 }
